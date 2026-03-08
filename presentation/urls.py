ac156a20270a706f25ebac23104ab0b624abf1b2
imhsi17 <calderonhuaynates@gmail.com>
2026-03-08 00:24:23 -0500

    add contact url

--- presentation/urls.py
+++ presentation/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, projects
+from .views import index, projects, contact
 
 urlpatterns = [
     path('', index),
-    path('proyectos/', projects)
+    path('proyectos/', projects),
+    path('contacto/', contact)
 ]
