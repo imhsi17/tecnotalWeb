c2dea9af4ce0a297fda09d5d8d0396de63224dc8
imhsi17 <calderonhuaynates@gmail.com>
2026-03-08 18:44:28 -0500

    add send email settings

--- tecnotalWeb/settings.py
+++ tecnotalWeb/settings.py
@@ -125,5 +125,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email backend configuration for development
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "calderonhuaynates@gmail.com"
+EMAIL_HOST_PASSWORD = "afdidnowyztmbiko"
+EMAIL_USE_TLS = True
